f79530cec671061bbc26a5a15f42fd75557746e7
Ritik Singh <ritik.singh@spit.ac.in>
2020-10-25 16:49:09 +0530

    integrated email

--- Boxoffice/booking/views.py
+++ Boxoffice/booking/views.py
@@ -8,7 +8,10 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from theater.models import Show
-from .models import Seat
+from .models import Seat,BookedSeat
+
+from Boxoffice.settings import EMAIL_HOST_USER
+from django.core.mail import send_mail
 
 class BookingView(GenericAPIView):
     serializer_class = BookingSerializer
@@ -22,19 +25,33 @@ class BookingView(GenericAPIView):
         booked_seat_obj = serializer.validated_data["booked_seat_obj"]
         return Response({"status": 'True' }, status=200)
 
-# class BookingEmailView(GenericAPIView):
-#     serializer_class = BookingSerializer
+class BookingEmailView(GenericAPIView):
+    serializer_class = BookingSerializer
 
-#     authentication_classes = [TokenAuthentication]
-#     permission_classes =[IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+    permission_classes =[IsAuthenticated]
     
-#     def get(self, request,id,pk):
-#     	seat_obj = Seat.objects.get
-#     	booking = Booking.objects.filter()
-#         serializer = BookingSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         booked_seat_obj = serializer.validated_data["booked_seat_obj"]
-#         return Response({"status": 'True' }, status=200)
+    def get(self, request,id,pk):
+
+    	user_obj = User.objects.get(id=id)
+    	seat_obj = Seat.objects.filter(seat_id=pk).first()
+    	bookedseat_obj = BookedSeat.objects.filter(seat=seat_obj).filter(booking__paid_by=user_obj).first()
+
+    	email=user_obj.email
+    	subject = 'Booking Details'
+    	message = '\nSeat No: {}{}'.format(seat_obj.seat_no,seat_obj.seat_id)
+    	message += '\nMovie: {}'.format(seat_obj.show.movie.name)
+    	message += '\nTheater: {}'.format(seat_obj.show.theater.name)
+    	message += '\nPaid Amount: Rs. {}'.format(bookedseat_obj.booking.paid_amount)
+    	 
+    	print(message)
+    	print(email)
+    	recepient = email
+    	send_mail(subject,message, EMAIL_HOST_USER, [recepient], fail_silently = False)
+
+    	print('jello')
+
+    	return Response({"status": 'True' }, status=200)
 
 class SeatView(APIView):
 	authentication_classes = [TokenAuthentication]
